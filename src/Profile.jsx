68fa249895a1106d82a8afa7951eef0dd923848f
Viktoria <doduchenko29@gmail.com>
2021-07-07 16:29:26 +0300

    add

---
+++ src/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import moment from 'moment'
+
+const Profile = (props) => {
+  const age = moment(props.birthDate).format('DD MMM YY');
+  return (
+    <div className="profile">
+      <div className="profile__name">{`${props.firstName} ${props.lastName}`}</div>
+      <div className="profile__birth">{`Was born ${age} in ${props.birthPlace}`}</div>
+    </div>
+  );
+};
+
+export default Profile;
+
+/*
+//вивести місяць строкой
+const b = new Date('1991-01-17T11:11:11.819Z').toLocaleString('eng', {
+  month: 'short',
+});
+
+console.log(b)*/
---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './Profile.jsx';
+import './index.scss';
+
+const rootElemnt = document.querySelector('#root');
+
+/*const userData = {
+  firstName: 'John',
+  lastName: 'Doe',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London',
+};*/
+
+ReactDOM.render(
+  <Profile
+    firstName="John"
+    lastName="Doe"
+    birthDate="1991-01-17T11:11:11.819Z"
+    birthPlace="London"
+  />,
+  rootElemnt
+);
